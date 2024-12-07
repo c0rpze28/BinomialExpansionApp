8affa0c3e5bf520d4899c34cb147d54ec75cb171
Angelo Corpuz <gaffudcorpuz@gmail.com>
2024-12-07 22:30:19 +0800

    testing button clicks

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,30 @@
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
 
 namespace BiCal;
 
 public partial class MainWindow : Window
 {
+    private int _clickCount = 0;
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    private void OnButtonClick(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        _clickCount++;
+
+        var greetingText = this.FindControl<TextBlock>("greetingText");
+        if (greetingText != null)
+        {
+            greetingText.Text = $"Button Clicked! {_clickCount}";
+        }
+        
     }
 }
